0127b2c4cd39cc9575e9c97a1d0ed2384b5e21f1
hugo2f <103283608+hugo2f@users.noreply.github.com>
2024-07-22 16:30:54 +0800

    refactor code

--- src/comparing.py
+++ src/comparing.py
@@ -1,61 +1,46 @@
 import copy
 import difflib
 import json
+from json import JSONDecodeError
 
 from deepdiff import DeepDiff
 import streamlit as st
 
-__all__ = ['character_level_compare_and_display', 'json_compare_and_display', 'json_accuracy_score']
+from html_formatting import PROMPT_CSS, RESPONSE_CSS, add_html_wrapping
+
+__all__ = ['character_level_compare_and_display', 'path_to_keys', 'follow_path',
+           'get_json_diffs', 'json_compare_and_display', 'json_accuracy_score',
+           'load_json_string']
 
 # -----------------------------------------------------------------------------
-# Private Globals
+# private globals
 # -----------------------------------------------------------------------------
 
 # types of json differences
 _VALUE_CHANGED = 0
 _KEY_CHANGED = 1
 
-_COLOR_CSS = """
-.red {
-    color: #ff7b72;
-}
-.green {
-    color: #2f6f37;
-}
-"""
 
-# leading and trailing newlines are needed, or else
-# add_html_formatting can return things like </style><div>..</div>,
-# which doesn't apply the styles properly
-_PROMPT_CSS = f"""
-<style>
-.prompt-block {{
-    background-color: #444;
-    font-family: Helvetica;
-    padding: 10px;
-    border-radius: 5px;
-    white-space: pre-wrap; /* preserve spaces */
-}}
-{_COLOR_CSS}
-</style>
-"""
-
-_RESPONSE_CSS = f"""
-<style>
-.response-block {{
-    background-color: #444;
-    font-family: monospace;
-    padding: 10px;
-    border-radius: 5px;
-    white-space: pre-wrap; /* preserve spaces */
-}}
-{_COLOR_CSS}
-</style>
-"""
+def load_json_string(json_string):
+    """
+    convert a string with a JSON into a dict
+    returns None if string is not a valid JSON
+    """
+    try:
+        converted_json = json.loads(json_string)
+        return converted_json
+    except (JSONDecodeError, TypeError):
+        return None
 
 
-def _add_html_wrapping(text, styles, classname):
-    return f"{styles}<div class='{classname}'>{text}</div>"
+def get_json_diffs(cur_dict, target_dict):
+    """
+    :param cur_dict: cur_response (converted to dict already)
+    :param target_dict: right answer
+    :return:
+        DeepDiff diffs with view='tree'
+    """
+    return DeepDiff(cur_dict, target_dict, view='tree')
 
 
 def character_level_compare_and_display(text1, text2, col1, col2):
@@ -64,7 +49,6 @@ def character_level_compare_and_display(text1, text2, col1, col2):
     :param text1, text2: Texts to compare
     :param col1, col2: target streamlit columns
     """
-
     matcher = difflib.SequenceMatcher(None, text1, text2)
     # differences as a list of tuples (operation, start1, end1, start2, end2)
     opcodes = matcher.get_opcodes()
@@ -78,7 +62,7 @@ def character_level_compare_and_display(text1, text2, col1, col2):
             elif tag in ('replace', 'delete'):
                 processed_text1.append(f"<span class='red'>{chunk}</span>")
 
-        html_code = _add_html_wrapping(''.join(processed_text1), _PROMPT_CSS, 'prompt-block')
+        html_code = add_html_wrapping(''.join(processed_text1), PROMPT_CSS, 'prompt-block')
         st.markdown(html_code, unsafe_allow_html=True)
 
     with col2:
@@ -90,20 +74,23 @@ def character_level_compare_and_display(text1, text2, col1, col2):
             elif tag in ('replace', 'insert'):
                 processed_text2.append(f"<span class='green'>{chunk}</span>")
 
-        html_code = _add_html_wrapping(''.join(processed_text2), _PROMPT_CSS, 'prompt-block')
+        html_code = add_html_wrapping(''.join(processed_text2), PROMPT_CSS, 'prompt-block')
         st.markdown(html_code, unsafe_allow_html=True)
 
 
-def _diff_to_path(diff):
+def path_to_keys(diff):
     """
     for example, diff could look like: "root['工单信息'][0]['产品名称']"
     we want the keys as a list: [工单信息, 0, 产品名称]
     the .replace("'", "") removes redundant single quotes for string keys
+
+    :param diff: item DeepDiff comparison, e.g."root['工单信息'][0]['产品名称']"
+    :return: deepdiff
     """
     return diff.strip("root[").strip("]").replace("'", "").split("][")
 
 
-def _follow_path(cur_dict, diff_keys):
+def follow_path(cur_dict, diff_keys):
     """follow the keys in diff_keys into cur_dict"""
     for key in diff_keys:
         # if the json contains lists, the key is an int index
@@ -123,14 +110,13 @@ def _highlight_json_diffs(d, diffs, color_class, change_type):
                         KEY_CHANGED to highlight the "subjson" rooted at a key
     :return: dictionary with HTML formatting applied
     """
-
     original_dict = copy.deepcopy(d)
 
     for diff in diffs:
-        diff_keys = _diff_to_path(diff)
+        diff_keys = path_to_keys(diff)
         # follow the keys until the second last level, so we can modify key_to_change
         key_to_change = diff_keys.pop()
-        cur_dict = _follow_path(original_dict, diff_keys)
+        cur_dict = follow_path(original_dict, diff_keys)
 
         if change_type == _VALUE_CHANGED:
             # only format the value
@@ -146,36 +132,26 @@ def _highlight_json_diffs(d, diffs, color_class, change_type):
     return original_dict
 
 
-def _load_json_string(json_string):
-    """
-    convert a string with a JSON into a dict
-    returns None if string is not a valid JSON
-    """
-
-    try:
-        converted_json = json.loads(json_string)
-    except json.decoder.JSONDecodeError:
-        return None
-    return converted_json
-
-
-def json_compare_and_display(text1, text2, col1, col2):
+def json_compare_and_display(text1, text2, col1, col2, warn1, warn2):
     """
     compare texts and display to streamlit columns
     :param text1, text2: JSON strings to compare
     :param col1, col2: target streamlit columns
+    :param warn1, warn2: empty() elements for displaying warning
     """
-
     # compare the json strings as json
-    dict1 = _load_json_string(text1)
-    dict2 = _load_json_string(text2)
+    dict1 = load_json_string(text1)
+    dict2 = load_json_string(text2)
 
     if dict1 is None or dict2 is None:
-        st.warning('At least one of the responses are not JSONs')
-        with col1:
-            st.write(text1)
-        with col2:
-            st.write(text2)
+        if dict1 is None:
+            with col1:
+                st.write(text1)
+                warn1.warning('Previous response does not contain a valid JSON')
+        if dict2 is None:
+            with col2:
+                st.write(text2)
+                warn2.warning('Current response does not contain a valid JSON')
         return
 
     diffs = DeepDiff(dict1, dict2, view='tree')
@@ -194,17 +170,17 @@ def json_compare_and_display(text1, text2, col1, col2):
     dict2_formatted = _highlight_json_diffs(dict2_formatted, changed, 'green', _VALUE_CHANGED)
 
     with col1:
-        html_code = _add_html_wrapping(
+        html_code = add_html_wrapping(
             json.dumps(dict1_formatted, indent=2, ensure_ascii=False),
-            _RESPONSE_CSS,
+            RESPONSE_CSS,
             'response-block')
 
         st.markdown(html_code, unsafe_allow_html=True)
 
     with col2:
-        html_code = _add_html_wrapping(
+        html_code = add_html_wrapping(
             json.dumps(dict2_formatted, indent=2, ensure_ascii=False),
-            _RESPONSE_CSS,
+            RESPONSE_CSS,
             'response-block')
         st.markdown(html_code, unsafe_allow_html=True)
 
@@ -219,14 +195,22 @@ def _count_values(json_obj):
         return 1
 
 
-def json_accuracy_score(cur, target):
+def json_accuracy_score(cur, target_dict):
     """
-    compare two JSON objects and return percentage of correct values matched
-    returns percentage with one decimal digit, ex: 12.3%
+    computes similarity of cur to target based on number of matched values in target
+
+    :param cur: string response
+    :param target_dict: dict
+    :return:
+        -1 if cur_dict or target_dict is None (response did not contain valid json)
+        percentage with one decimal digit, ex: 12.3
     """
-    # convert JSON strings to dicts
-    cur_dict = json.loads(cur)
-    target_dict = json.loads(target)
+
+    # convert JSON string to dict
+    try:
+        cur_dict = json.loads(cur)
+    except (JSONDecodeError, TypeError):
+        return -1
 
     # number of correct values to match
     target_value_count = _count_values(target_dict)
@@ -239,45 +223,53 @@ def json_accuracy_score(cur, target):
 
     not_matched = 0
     for diff in added + changed:
-        diff_keys = _diff_to_path(diff)
+        diff_keys = path_to_keys(diff)
         # follow the keys until the second last level, so we can modify key_to_change
-        cur_dict = _follow_path(target_dict, diff_keys)
+        cur_dict = follow_path(target_dict, diff_keys)
         not_matched += _count_values(cur_dict)
 
     accuracy = 1 - not_matched / target_value_count
-    return f'{accuracy * 100:.1f}%'
+    return round(accuracy * 100, 1)
+
+
+def main():
+    """testing use"""
+    json1 = '''
+        {
+          "key1": {
+            "nested_key1": ["value1", "value2"],
+            "nested_key3": "value2"
+          },
+          "key2": {
+            "nested_key3": "value3"
+          },
+          "key3": {
+            "nested_key4": "value4"
+          }
+        }
+        '''
+
+    json2 = '''
+        {
+          "key1": {
+            "nested_key1": "value1",
+            "nested_key2": "value2"
+          },
+          "key2": {
+            "nested_key3": "value3",
+            "nested_key4": "value4"
+          }
+        }
+        '''
+
+    acc = json_accuracy_score(json1, json2)
+    diffs = DeepDiff(json.loads(json1), json.loads(json2), view='tree')
+    added = [diff.path() for diff in diffs.get('dictionary_item_added', [])]
+    changed = ([diff.path() for diff in diffs.get('values_changed', [])]
+               + [diff.path() for diff in diffs.get('type_changes', [])])
+    print(added)
+    print(changed)
 
 
 if __name__ == '__main__':
-    """testing use"""
-
-    json1 = '''
-    {
-      "key1": {
-        "nested_key1": ["value1", "value2"],
-        "nested_key3": "value2"
-      },
-      "key2": {
-        "nested_key3": "value3"
-      },
-      "key3": {
-        "nested_key4": "value4"
-      }
-    }
-    '''
-
-    json2 = '''
-    {
-      "key1": {
-        "nested_key1": "value1",
-        "nested_key2": "value2"
-      },
-      "key2": {
-        "nested_key3": "value3",
-        "nested_key4": "value4"
-      }
-    }
-    '''
-
-    accuracy = json_accuracy_score(json1, json2)
-    print(accuracy)
+    main()
